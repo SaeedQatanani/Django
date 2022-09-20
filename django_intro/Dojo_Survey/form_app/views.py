0cdac98f5a6b792e02c55f3c5705e8acb4d2a354
SaeedQatanani <saeedqatananil@gmail.com>
2022-09-20 17:05:22 +0300

    submission

---
+++ django_intro/Dojo_Survey/form_app/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, redirect
+
+def index(request):
+    return render(request, 'index.html')
+
+def show(request):
+    context = {
+        'name' : request.POST['name'],
+        'location' : request.POST['location'],
+        'language' : request.POST['language'],
+        'comment' : request.POST['comment'],
+    }
+    
+    return render(request, 'result.html', context)
+
+
+
