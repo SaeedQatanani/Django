c02f752a2ef8cb42ee8fd665db76ef168d4df9e3
SaeedQatanani <saeedqatananil@gmail.com>
2022-09-21 11:36:47 +0300

    inital submisssion

---
+++ django_intro/counter/app_one/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, redirect
+
+def count(request):
+    if 'counter' not in request.session:
+        request.session['counter'] = 0
+    request.session['counter'] += 1
+    return render(request, 'index.html')
+
+def destroy(request):
+    del request.session['counter']
+    return redirect('/')
