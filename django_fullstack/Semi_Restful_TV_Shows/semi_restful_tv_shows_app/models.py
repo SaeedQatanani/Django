96122540750223b591b2a032fd35a441748fbe8a
SaeedQatanani <saeedqatananil@gmail.com>
2022-09-28 14:44:19 +0300

    validated

--- django_fullstack/Semi_Restful_TV_Shows/semi_restful_tv_shows_app/models.py
+++ django_fullstack/Semi_Restful_TV_Shows/semi_restful_tv_shows_app/models.py
@@ -1,4 +1,24 @@
 from django.db import models
+import time
+import datetime
+
+class ShowManager(models.Manager):
+    def validator(self, postData):
+        errors = {}
+        for show in Show.objects.all():
+            if show.title == postData['title']:
+                errors["title"] = "A show's title should be Unique"
+        if len(postData['title']) < 2:
+            errors["name"] = "A show's title should be at least two characters"
+        if len(postData['network']) < 3:
+            errors["network"] = "A show's network should be at least three characters"
+        if postData['description'] :
+            if len(postData['description']) < 10:
+                errors["description"] = "A show's description should be at least ten characters"
+        if postData['release_date'] > datetime.date.today().isoformat():
+            errors["release_date"] = "A show's release date should be in the past"
+        return errors
+
 
 class Show(models.Model):
     title = models.CharField(max_length=255)
@@ -7,4 +27,5 @@ class Show(models.Model):
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = ShowManager()
 
